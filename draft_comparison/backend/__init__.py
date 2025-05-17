0524dc60a7e737ec8ee7bb77c98d6aec7c8f83e5
gyasis <gyasis@gmail.com>
2025-05-16 23:10:02 -0400

    Remove specific files from git tracking while keeping them locally

--- draft_comparison/backend/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Chain of Draft Comparison Backend
-
-This package contains the WebSocket server implementation for the comparison tool.
-"""
-
-from .comparison_server import ComparisonServer
-
-__all__ = ['ComparisonServer'] 
--- tests/test_utils.py
+++
@@ -1,3 +0,0 @@
-import pytest
-
-from src.utils.utils import *
